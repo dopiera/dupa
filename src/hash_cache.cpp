ad87f70800ea027516ba0f9954282532b2123b1a
Marek Dopiera <marek@dopiera.pl>
2016-08-14 23:54:38 +0100

    Remove stale protobuf includes.

--- src/hash_cache.cpp
+++ src/hash_cache.cpp
@@ -12,9 +12,6 @@
 
 #include <boost/functional/hash/hash.hpp>
 
-#include <google/protobuf/io/coded_stream.h>
-#include <google/protobuf/io/zero_copy_stream_impl.h>
-
 #include "exceptions.h"
 #include "log.h"
 #include "sql_lib.h"
