bc0c9d2e9a3a277d7a09da4bad75ab79bc55d689
Marek Dopiera <marek@dopiera.pl>
2018-07-15 17:45:56 +0200

    Modify tidy.sh to work on indiviual files.

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,7 +1,15 @@
 #/bin/sh
 
+CHECKS='-*,clang-analyzer-*,google-*,modernize-*,misc-*,performance-*,portability-*,readability-*,-google-runtime-references,-readability-implicit-bool-conversion'
+
 PROJ_DIR="$(dirname "$0")/.."
-cd "${PROJ_DIR}"
-clang-tidy -checks='-*,clang-analyzer-*,google-*,modernize-*,misc-*,performance-*,portability-*,readability-*,-google-runtime-references,-readability-implicit-bool-conversion' -p . -header-filter=src/.* src/*.cpp
+
+if [ $# -eq 0 ] ; then
+  clang-tidy -checks="$CHECKS" -p "$PROJ_DIR" -header-filter=src/.* \
+    $PROJ_DIR/src/*.cpp
+else
+  clang-tidy -checks="$CHECKS" -p "$PROJ_DIR" -header-filter=src/.* \
+    "$@"
+fi
 
 
