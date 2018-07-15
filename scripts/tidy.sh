c3469ba79a82a9257f8930e1c6e03c27d52034bd
Marek Dopiera <marek@dopiera.pl>
2018-07-16 00:12:31 +0200

    Change naming to Google's guidelines.
    Also add clang-tidy config to stick to that in the future.
    
    There is one discrepancy: structs' members are enforced to end with an
    underscore despite Google not enforcing it. clang-tidy can't
    differentiate between classes and structs, though, so it's not possible
    to implement it properly at this point.

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,14 +1,12 @@
 #/bin/sh
 
-CHECKS='-*,clang-analyzer-*,google-*,modernize-*,misc-*,performance-*,portability-*,readability-*,-google-runtime-references,-readability-implicit-bool-conversion'
-
 PROJ_DIR="$(dirname "$0")/.."
 
 if [ $# -eq 0 ] ; then
-  clang-tidy -checks="$CHECKS" -p "$PROJ_DIR" -header-filter=src/.* \
+  clang-tidy -format-style=google -p "$PROJ_DIR" -header-filter=src/.* \
     $PROJ_DIR/src/*.cpp
 else
-  clang-tidy -checks="$CHECKS" -p "$PROJ_DIR" -header-filter=src/.* \
+  clang-tidy -format-style=google -p "$PROJ_DIR" -header-filter=src/.* \
     "$@"
 fi
 
